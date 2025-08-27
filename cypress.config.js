0f499a9fc0494c9de02451eff98427cff7306605
enveryasar <enver.yasar@bynder.com>
2025-08-27 13:52:29 +0200

    results merge

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,18 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/grep/src/plugin')(config);
+      return config;
     },
     video: true,
-    videoUploadOnPasses: true
+    videoUploadOnPasses: true,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      reporterEnabled: "mocha-junit-reporter, spec",
+      mochaJunitReporterReporterOptions: {
+        mochaFile: "cypress/results/junit/results-[hash].xml",
+        toConsole: true,
+      }
+    }
   },
 });
